6ff9bd0a557658b5b2bee6b93482c118d2a12bca
zhangcunli <zhangcunli@youzan.com>
2017-06-15 20:24:08 +0800

    Mod: 修改 icon

--- gen-doc.sh
+++ gen-doc.sh
@@ -4,3 +4,10 @@ rm -rf /tmp/phpdoc-cache-*
 rm -rf ./build
 phpdoc --target ./build --directory ./src/ --cache-folder /tmp
 
+cp -f ./helper/favicon.ico  build/images/favicon.ico
+
+file1="./helper/clear.js"
+file2="./build/js/bootstrap.min.js"
+file3="./build/js/temp"
+(cat "$file1" | cat - "$file2" > "$file3") && mv "$file3" "$file2"
+
